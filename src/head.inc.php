7ffc670099c7dcde8b3dd1742fbf6cc2962ffc89
Vaugarni ryan <ryan.vaugarni@gmail.com>
2022-06-05 15:37:49 +0200

    update icon

--- src/head.inc.php
+++ src/head.inc.php
@@ -15,10 +15,9 @@ include_once "./src/get_history_user.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PPE MAISON DES LIGUES</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/asset/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/asset/favicon-16x16.png">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="./asset/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./asset/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./asset/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     <link rel="stylesheet" href="<?= $_new_css->_css ?>">
 </head>
